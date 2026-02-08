5b594da46cb14954b241608dee2651dfdf57dd32
JavierG <javiergilleon14@gmail.com>
2026-02-08 20:05:37 +0100

    Act

--- create.sh
+++ create.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
-echo -n "Enter EFI size: "
-read EFI
+set -e
 
-echo -n "Enter SWAP size: "
-read SWAP
+EFI_SIZE_MB=1024
+RAM_SIZE_KB=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
+RAM_SIZE_MB=$((RAM_SIZE_KB / 1024))
 
-echo "label: gpt" >> partition.dump
-echo "start=2048, size=$EFI, type=uefi" >> partition.dump
-echo "size=$SWAP, type=swap" >> partition.dump
-echo "type=linux" >> partition.dump
-
-SDA=0
-VDA=0
-
-(ls /dev/ | grep sda ) && SDA=1
-(ls /dev/ | grep vda ) && VDA=1
-
-if [SDA]; then 
-  sfdisk /dev/sda < partition.dump
-elif [VDA]; then
-  sfdisk /dev/vda < partition.dump
+if   [ "$RAM_SIZE_MB" -le 4096 ];  then
+  SWAP_MB=$((RAM_SIZE_MB*2))
+elif [ "$RAM_SIZE_MB" -le 8192 ];  then
+  SWAP_MB=$((RAM_SIZE_MB))
+elif [ "$RAM_SIZE_MB" -le 16384 ]; then
+  SWAP_MB=8192
 else
-  echo "Disk not detected"
+  SWAP_MB=4096
 fi
 
+
+cat > partition.dump << EOF
+label: gpt
+start=2048, size=${EFI_SIZE_MB}M, type=uefi
+size=${SWAP_MB}M, type=swap
+type=linux
+EOF
+
+if   [ -b /dev/sda ]; then
+  DISK=/dev/sda
+elif [ -b /dev/vda ]; then
+  DISK=/dev/vda
+else
+  echo "Disk not found"
+  exit 1
+fi
+
+sfdisk "$DISK" < partition.dump
