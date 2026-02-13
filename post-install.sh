254f2ffc570931d7121e5e01b8640438c205ba94
JavierG <javiergilleon14@gmail.com>
2026-02-13 11:51:56 +0100

    .

--- post-install.sh
+++ post-install.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo "Hola"
+echo "-------------------------------------"
+echo "Installing yay..."
+echo "-------------------------------------"
+
+cd /tmp
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+echo "-------------------------------------"
+echo "yay Successfully installed"
+echo "-------------------------------------"
+
+echo "-------------------------------------"
+echo "Installing Hyprland..."
+echo "-------------------------------------"
+cd /tmp
+yay -S ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite libxrender libxcursor pixman wayland-protocols cairo pango libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang-git hyprcursor-git hyprwayland-scanner-git hyprwire-git xcb-util-errors hyprutils-git glaze hyprgraphics-git aquamarine-git re2 hyprland-qtutils-git muparser
+git clone --recursive https://github.com/hyprwm/Hyprland
+cd Hyprland
+make all && sudo make install
